12b0d2a94438d255dbdececdd6b53cf243edc604
nojimage <nojimage@gmail.com>
2011-07-14 19:28:40 +0900

    activate DebugKit plugin

--- app/app_controller.php
+++ app/app_controller.php
@@ -2,6 +2,7 @@
 
 class AppController extends Controller {
 
+    public $components = array('DebugKit.Toolbar' => array('panels' => array('history' => false)));
     public $view = 'Taltal.Phptal';
 
 }
